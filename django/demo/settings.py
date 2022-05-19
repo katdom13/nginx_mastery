0db8c60f077b5d3ca3f840a3a7565dd5e0a73424
katdom13 <katdom13@gmail.com>
2022-05-19 19:44:47 +0800

    Nginx and django caching controls
    Identify the default behaviour of Nginx serving django webpages from
    cache, understand the relevance of the vary header and its role on cache
    key generation, configure nginx to ignore specific header data,
    configure which request headers a cache mechanism should take into
    account when building its cache key and control how caching is performed
    using django header directives.

--- django/demo/settings.py
+++ django/demo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "demo.app.apps.AppConfig",
 ]
 
 MIDDLEWARE = [
--- django/demo/urls.py
+++ django/demo/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+
+from demo.app.views import p1, p2
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("p1/", TemplateView.as_view(template_name="p1.html")),
-    path("p2/", TemplateView.as_view(template_name="p2.html")),
+    path("p1/", p1),
+    path("p2/", p2),
 ]
